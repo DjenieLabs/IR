8cfa2b414a8ef1a337692ec1df81605046eca515
Alexander Agudelo <mario323qp@gmail.com>
2017-11-13 18:23:35 +1000

    Increased version to 1.3.0

--- dist/libs/helpers.js
+++
@@ -1,1 +0,0 @@
-"use strict";define(function(){return{inRange:function(e,n){return Number(e)>.7*Number(n)&&Number(e)<1.3*Number(n)},millis:function(){return(new Date).getTime()},isNumeric:function(e){return!isNaN(parseFloat(e))&&isFinite(e)}}});
